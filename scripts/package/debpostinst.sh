1a60b41dcbf6c7879c239f47a2c6cd775a086b7f
Jeff Epler <jepler@gmail.com>
2020-04-28 21:09:00 -0500

    add/remove diverts like rpi-k

---
+++ scripts/package/debpostinst.sh
@@ -0,0 +1,32 @@
+cat <<EOF
+#!/bin/sh
+
+set -e
+
+# Pass maintainer script parameters to hook scripts
+export DEB_MAINT_PARAMS="\$*"
+
+# Tell initramfs builder whether it's wanted
+export INITRD=$want_initrd
+
+mkdir -p /usr/share/rpikernelhack/overlays
+mkdir -p /boot/overlays
+
+EOF
+
+(cd $tmpdir/boot; find . -type f) | while read fn; do
+	dfn=/usr/share/rpikernelhack/"$fn"
+cat <<EOF
+if [ -f "$dfn" ]; then
+	rm -f "/boot/$fn"
+	dpkg-divert --package rpikernelhack --rename --remove "/boot/$fn"
+	sync
+fi
+EOF
+done
+
+cat <<EOF
+
+test -d $debhookdir/$script.d && run-parts --arg="$version" --arg="/$installed_image_path" $debhookdir/$script.d
+exit 0
+EOF
